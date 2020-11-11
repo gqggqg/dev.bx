6286799accfb12622efd72fbe7396e2fd16c0d78
gg <71404862+gqggqg@users.noreply.github.com>
2020-11-11 17:51:36 +0200

    task solution

---
+++ Tasks/task_2.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * TDD
+ */
+
+require_once 'assert_equals.php';
+require_once 'console_read.php';
+
+$variables = [1, -5, 0.5, +1e6, true, false, 'test_string'];
+foreach ($variables as $value)
+{
+	$parameter = is_string($value) ? $value : var_export($value, true);
+	assertEquals($value, readFromConsole('', $parameter), "readFromConsole('', {$parameter}) return " . varDumbToString($value));
+}
+
+function varDumbToString($var = null)
+{
+	ob_start();
+	var_dump($var);
+	$content = trim(ob_get_contents());
+	ob_end_clean();
+	return $content;
+}
