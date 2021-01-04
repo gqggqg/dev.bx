7f064c68103c23bbc4beb25a2d1dd6c5ff9b4d15
gg <71404862+gqggqg@users.noreply.github.com>
2021-01-04 20:35:48 +0200

    task 7: fixed 2 and 5 queries

--- Tasks/Task_7 [Database]/db_queries.sql
+++ Tasks/Task_7 [Database]/db_queries.sql
@@ -5,7 +5,7 @@ FROM author a
 GROUP BY a.NAME;
 
 # суммарный остаток книг каждого автора во всех магазинах
-SELECT NAME, CITY, COUNT(*)
+SELECT NAME, CITY, SUM(QUANTITY)
 FROM book_store bs
 	     INNER JOIN author_book ab on bs.BOOK_ID = ab.BOOK_ID
 	     INNER JOIN author a on ab.AUTHOR_ID = a.ID
@@ -28,25 +28,10 @@ FROM book_store bs
 GROUP BY CITY, b.NAME;
 
 # разница между остатком книг в Калининграде и Черняховске
-SELECT NAME as BOOK_TITLE,
-       KGD_CHR.KGD_QUANTITY as NUMBER_OF_BOOKS_IN_KALININGRAD ,
-       KGD_CHR.CHR_QUANTITY as NUMBER_OF_BOOKS_IN_CHERNYAKHOVSK,
-       ABS(KGD_CHR.KGD_QUANTITY - KGD_CHR.CHR_QUANTITY) as DIFFERENCE_IN_QUANTITY
-FROM book b
-	     INNER JOIN
-     (
-	     SELECT BOOK_ID, CITY, IFNULL(QUANTITY, 0) as KGD_QUANTITY, CHR_C, IFNULL(CHR_Q, 0) as CHR_QUANTITY
-	     FROM (
-		          SELECT BOOK_ID, CITY, QUANTITY
-		          FROM book_store bs
-			               INNER JOIN store s ON bs.STORE_ID = s.ID
-		          WHERE s.CITY = 'Калининград'
-	          ) KGD_BOOKS
-		          LEFT JOIN
-	          (
-		          SELECT BOOK_ID as CHR_B, CITY as CHR_C, QUANTITY as CHR_Q
-		          FROM book_store bs
-			               INNER JOIN store s ON bs.STORE_ID = s.ID
-		          WHERE s.CITY = 'Черняховск'
-	          ) CHR_BOOKS ON KGD_BOOKS.BOOK_ID = CHR_BOOKS.CHR_B
-     ) KGD_CHR ON b.ID = KGD_CHR.BOOK_ID
+SELECT b.NAME,
+		SUM(IF(STORE_ID = 1, QUANTITY, 0)) AS QUANTITY_IN_KALININGRAD,
+        SUM(IF(STORE_ID = 2, QUANTITY, 0)) AS QUANTITY_IN_CHERNYAHOVSK,
+        SUM(IF(STORE_ID = 1, QUANTITY, 0)) - SUM(IF(STORE_ID = 2, QUANTITY, 0)) AS DIFFERENCE
+FROM book_store bs
+		INNER JOIN book b on bs.BOOK_ID = b.ID
+GROUP BY bs.BOOK_ID;
